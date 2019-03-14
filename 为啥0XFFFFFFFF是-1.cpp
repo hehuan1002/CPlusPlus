a15105a88f6754721dc556f69a56ac428a643be2
hehuan1002 <hehuan1002@126.com>
2019-03-14 16:12:29 +0800

    Create 为啥0XFFFFFFFF是-1.cpp

---
+++ 为啥0XFFFFFFFF是-1.cpp
@@ -0,0 +1,16 @@
+// Test0Xffffffff.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "windows.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// 实际上是0xFFFFFFFF 
+	DWORD dw = -1;
+	unsigned int d = -1;
+
+	// 计算机中的数据都是以补码形式存储的，结果？？？
+	return 0;
+}
+
