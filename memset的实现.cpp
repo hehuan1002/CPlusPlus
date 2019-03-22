72ab12aa93b912e4d9127cf37d005d7418b825d1
hehuan1002 <hehuan1002@126.com>
2019-03-22 15:49:10 +0800

    Update memset的实现.cpp

--- memset的实现.cpp
+++ memset的实现.cpp
@@ -1,1 +1,55 @@
+//// TestMemset.cpp : 定义控制台应用程序的入口点。
 //
+
+#include "stdafx.h"
+#include <memory.h>
+struct TKUSERREQHEMATINIC
+{
+	int n;
+	int m;
+};
+
+
+void * __cdecl tk_memset (
+	void *dst,
+	int val,
+	size_t count
+	)
+{
+	char *start = (char*)dst;
+
+#if defined (_M_IA64) || defined (_M_AMD64)
+
+	{
+
+
+		__declspec(dllimport)
+
+
+			void RtlFillMemory( void *, size_t count, char );
+
+		RtlFillMemory( dst, count, (char)val );
+
+	}
+
+#else  /* defined (_M_IA64) || defined (_M_AMD64) */
+	while (count--) {
+		*start = (char)val;
+		start = start + 1;
+	}
+#endif  /* defined (_M_IA64) || defined (_M_AMD64) */
+
+	return(dst); // 返回首地址
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TKUSERREQHEMATINIC stAck;
+	tk_memset(&stAck, 0, sizeof(stAck));
+
+
+	//https://blog.csdn.net/qq_42592097/article/details/81947072 
+
+	return 0;
+}
+
