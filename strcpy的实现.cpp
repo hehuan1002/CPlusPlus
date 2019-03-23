bf7d81a1ebd2e2fa71d81fc17294570c23c713b3
huan he <hehuan1002@126.com>
2019-03-23 16:12:41 +0800

    Update strcpy的实现.cpp

--- strcpy的实现.cpp
+++ strcpy的实现.cpp
@@ -7,19 +7,21 @@
 
 char* HH_strcpy(char *dst, const char* src)
 {
-	/*assert(dst);
-	assert(src);
-	char* s = dst;
-	while (*s++ = *src++)
-	;
-	return dst;*/
-
-	// Error: 错误方式的拷贝，不理解
 	assert(dst);
 	assert(src);
-	while (*dst++ = *src++)
+	char* s = dst;// s是临时的指针变量，随着循环的进行，地址一直在变，直到循环结束。s的地址从0x006c6338变化到0x006c633c。
+	while (*s++ = *src++)
 		;
-	return dst;
+	return dst; // 必须传回首地址才行
+
+
+
+	//Error: 错误方式的拷贝，不理解，难道是数组的首地址
+	//assert(dst);
+	//assert(src);
+	//while (*dst++ = *src++)
+		;
+	//return dst; 随着循环的进行，dst地址一直在发生变化，return dst就不是返回首地址了。
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
