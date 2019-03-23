8c9253c8c049592f42eec846b87e29e76931e0cc
huan he <hehuan1002@126.com>
2019-03-23 16:14:57 +0800

    Update strcpy的实现.cpp

--- strcpy的实现.cpp
+++ strcpy的实现.cpp
@@ -9,8 +9,8 @@ char* HH_strcpy(char *dst, const char* src)
 {
 	assert(dst);
 	assert(src);
-	char* s = dst;// s是临时的指针变量，随着循环的进行，地址一直在变，直到循环结束。s的地址从0x006c6338变化到0x006c633c。
-	while (*s++ = *src++)
+	char* cp = dst;// s是临时的指针变量，随着循环的进行，地址一直在变，直到循环结束。s的地址从0x006c6338变化到0x006c633c。
+	while (*cp++ = *src++)
 		;
 	return dst; // 必须传回首地址才行
 
